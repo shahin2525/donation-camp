3027c582edbb23288d962f843a184a73b74e9231
shahin2525 <shahin451999@gmail.com>
2024-07-13 23:03:37 -0700

    add header

---
+++ src/components/Statistic/Statistic.jsx
@@ -0,0 +1,9 @@
+const Statistic = () => {
+  return (
+    <div>
+      <h1>statistic</h1>
+    </div>
+  );
+};
+
+export default Statistic;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import Statistic from "./components/Statistic/Statistic";
+import Donation from "./components/Donation/Donation";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistic",
+        element: <Statistic></Statistic>,
+      },
+      {
+        path: "/donation",
+        element: <Donation></Donation>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
