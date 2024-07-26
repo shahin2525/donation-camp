6589935accdb4db0fa685db88373bc07855e75fc
shahin2525 <shahin451999@gmail.com>
2024-07-25 23:27:41 -0700

    add firebase

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5zYWfu9641YLlL-csrU_f45l-lP15F-M",
+  authDomain: "practice-day-one-d676e.firebaseapp.com",
+  projectId: "practice-day-one-d676e",
+  storageBucket: "practice-day-one-d676e.appspot.com",
+  messagingSenderId: "401299949237",
+  appId: "1:401299949237:web:b09c4f7d667cb8fd03e0fc",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
