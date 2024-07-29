36408260ce1aaebc5f36deedec777b365a6ef803
shahin2525 <shahin451999@gmail.com>
2024-07-28 23:58:19 -0700

    add password login

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,38 @@
+// import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const SignUp = () => {
+  const auth = getAuth();
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
   return (
     <div>
-      <h1>Sign up</h1>
+      <div>
+        <h1 className="mt-20">Sign up</h1>
+        <form className="mt-20" onSubmit={handleRegister}>
+          <input type="email" name="email" placeholder="type email" />
+          <br />
+          <input type="password" name="password" placeholder="type password" />
+          <br />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
     </div>
   );
 };
