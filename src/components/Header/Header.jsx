8c84975ee68142cb8927a814319509cfb8b54399
shahin2525 <shahin451999@gmail.com>
2024-08-02 22:46:55 -0700

    add login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,19 +30,6 @@ const Header = () => {
         <div className="flex-none">
           <div className="mr-[48px] font-normal text-[18px]">
             <NavLink to={"/"}>Home</NavLink>
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg> */}
           </div>
           <div className="mr-[48px] font-normal text-[18px]">
             <NavLink to="/donation">Donation</NavLink>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,176 +1,50 @@
-import {
-  getAuth,
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  sendPasswordResetEmail,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import { app } from "../../firebase/firebase.init";
-import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
 const Login = () => {
-  //
-  const [user, setUser] = useState(null);
-  const googleProvider = new GoogleAuthProvider();
-  const githubProvider = new GithubAuthProvider();
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [type, setType] = useState(false);
-  const emailRef = useRef();
-
-  const auth = getAuth(app);
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-
-    setError("");
-    setSuccess("");
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        // Signed up
-        console.log(result.user);
-
-        if (result.user.emailVerified) {
-          setSuccess("user login successfully");
-        } else {
-          alert("verify your email");
-        }
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        setError(errorMessage);
-        // ..
-      });
-  };
-
-  const handelSocialLogin = () => {
-    signInWithPopup(auth, googleProvider)
-      .then((result) => {
-        const loggedUser = result.user;
-        setUser(loggedUser);
-        console.log(loggedUser);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const handleSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        setUser(null);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const handleGithubLogin = () => {
-    signInWithPopup(auth, githubProvider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
-  };
-
-  const handleForgetPassword = (e) => {
-    e.preventDefault();
-    const email = emailRef.current.value;
-    if (!email) {
-      setError("please provide email");
-      return;
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      setError("please provide valid email ");
-      return;
-    }
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        alert("check your email");
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        // ..
-      });
-  };
   return (
     <div>
-      <div>
-        <div>
-          <div>
-            <h1 className="mt-10">Login page </h1>
-            {!user && (
-              <button className="mr-10 mt-10" onClick={handelSocialLogin}>
-                Google login
-              </button>
-            )}
+      <div className="hero bg-base-200 min-h-screen pt-20">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
           </div>
-
-          <div>
-            {!user && (
-              <button className="mr-10 mt-10" onClick={handleGithubLogin}>
-                Github-Login
-              </button>
-            )}
-          </div>
-
-          <div className="mt-10">
-            {user && (
-              <div>
-                <button onClick={handleSignOut}>Sign Out</button>
-                <h2>user-name:{user.displayName}</h2>
-                <p>Email:{user.email}</p>
-                <img src={user.photoURL} alt="" />
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
               </div>
-            )}
-          </div>
-          <div>
-            <span>do not have any account </span>
-            <button>
-              <Link to="/sign-up">sign-up</Link>
-            </button>
-          </div>
-        </div>
-        {/* login start */}
-        <div>
-          <div className="mt-4">
-            <h1>email login</h1>
-            <form className="mt-4" onSubmit={handleLogin}>
-              <input
-                type="email"
-                name="email"
-                placeholder="type email"
-                ref={emailRef}
-              />
-              <br />
-              <input
-                type={type ? "text" : "password"}
-                name="password"
-                placeholder="type password"
-              />
-              <span onClick={() => setType(!type)}>
-                {type ? "hide" : "show password"}
-              </span>
-              <br />
-              <input type="submit" value="submit" />
-              <br />
-              <label>
-                <a onClick={handleForgetPassword} href="">
-                  forgot password
-                </a>
-              </label>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
             </form>
           </div>
-          {error && <p>error:{error}</p>}
-          {success && <p>{success}</p>}
         </div>
       </div>
     </div>
