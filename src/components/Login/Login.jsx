5fcfe2e9a8c3e1a084a4a74024bc87389345917e
shahin2525 <shahin451999@gmail.com>
2024-08-02 23:45:20 -0700

    add use context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen pt-20">
@@ -12,13 +20,14 @@ const Login = () => {
             </p>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
+                  name="email"
                   placeholder="email"
                   className="input input-bordered"
                   required
@@ -31,6 +40,7 @@ const Login = () => {
                 <input
                   type="password"
                   placeholder="password"
+                  name="password"
                   className="input input-bordered"
                   required
                 />
@@ -43,6 +53,12 @@ const Login = () => {
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
+              <p>
+                do not have an account? please{" "}
+                <Link to="/sign-up">
+                  <button className="btn btn-primary">Sign up</button>
+                </Link>
+              </p>
             </form>
           </div>
         </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,104 +1,82 @@
-// import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
-
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  sendEmailVerification,
-  updateProfile,
-} from "firebase/auth";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [type, setType] = useState(false);
-  const auth = getAuth();
-  const handleRegister = (e) => {
+  const handleSignUp = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    const accepted = e.target.terms.checked;
-    setError("");
-    setSuccess("");
-    if (password.length < 6) {
-      setError("password will more 6 or equal");
-      return;
-    } else if (!/[A-Z]/.test(password)) {
-      setError("password will have at least a single upper case");
-      return;
-    } else if (!accepted) {
-      setError("please accept our terms and condition");
-      return;
-    }
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((result) => {
-        // Signed up
-        // const user = userCredential.user;
-        // update user profile
-
-        updateProfile(auth.currentUser, {
-          displayName: name,
-          photoURL: "https://example.com/jane-q-user/profile.jpg",
-        })
-          .then(() => {
-            console.log("updated user successfully");
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-
-        setSuccess("user created successfully");
-        sendEmailVerification(result.user).then(() => {
-          alert("check email for verify");
-        });
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        setError(errorMessage);
-        // ..
-      });
+    console.log(name, email, password);
   };
-  const handleShowPassword = () => {
-    setType(!type);
-  };
+
   return (
     <div>
-      <div className="mt-20">
-        <h1>Sign up</h1>
-        <form className="mt-20" onSubmit={handleRegister}>
-          <input type="text" name="name" placeholder="type your name" />
-          <br />
-          <input type="email" name="email" placeholder="type email" />
-          <br />
-          <input
-            type={type ? "text" : "password"}
-            name="password"
-            placeholder="type password"
-          />
-          <span onClick={handleShowPassword}>
-            {type ? "hide" : "show password"}
-          </span>
-          <br />
-
-          <input type="checkbox" placeholder="checkbox" name="terms" />
-          <label htmlFor="">Accept our terms and condition</label>
-          <br />
-          <input type="submit" value="submit" />
-        </form>
-        <div>
-          <p>
-            already have an account{" "}
-            <button>
-              <Link to="/login">Login</Link>
-            </button>
-          </p>
+      <div className="hero bg-base-200 min-h-screen pt-20">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSignUp} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Sign up</button>
+              </div>
+              <p>
+                already have an account? please
+                <Link to="/login">
+                  <button className="btn btn-primary">Login</button>
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-      {error && <p>error:{error}</p>}
-      {success && <p>{success}</p>}
     </div>
   );
 };
